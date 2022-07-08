05095519407accfe8240d34b88ebf2b6fbfc7174
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-07-08 03:10:06 +0200

    atualizacao no head da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import Clients from "../components/Clients/Clients";
 import Main from "../components/Main/Main";
@@ -7,6 +8,9 @@ import Register from "../components/Register/Register";
 function Home() {
   return (
     <>
+    <Head>
+      <title>Pagina inicial</title>
+    </Head>
       <Main />
       <Projects />
       <Clients />
